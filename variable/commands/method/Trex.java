0537db75bfa9b757caf2f1c8bde32c01c74ce924
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-12 14:48:20 +0530

    program was sucessfully ecuted

--- variable/commands/method/Trex.java
+++ variable/commands/method/Trex.java
@@ -1,9 +1,9 @@
-public class{
+public class Trex{
     public void add(int numone,int numtwo){
         int numthree = numone + numtwo;
         System.out.println(numthree);
     }
-    public static void main(string args[]){
+    public static void main(String args[]){
         Trex obj = new Trex();
         obj.add(50,10);
     }
