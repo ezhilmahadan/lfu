4f80b9974da28e32c39ace035880aa959b35e358
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-12 14:39:53 +0530

    $

---
+++ variable/commands/method/Trex.java
@@ -0,0 +1,10 @@
+public class{
+    public void add(int numone,int numtwo){
+        int numthree = numone + numtwo;
+        System.out.println(numthree);
+    }
+    public static void main(string args[]){
+        Trex obj = new Trex();
+        obj.add(50,10);
+    }
+}
