48060a46347c103d6b0aaaa8b46917b8bbbe8e1a
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-12 14:17:45 +0530

    program was sucessfully ecuted

--- variable/commands/Jk.java
+++ variable/commands/Jk.java
@@ -1,5 +1,8 @@
 public class Jk{
     public static void main(String args[]){
-        system.out.println("cat");
+        int num1 = 20;
+        int num2 = 40;
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
