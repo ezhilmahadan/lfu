a28f2335192dc858614cb215c50a1035b27c1df6
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 10:55:30 +0530

    program was sucessfully excuted

---
+++ condtionalsatement/Ben.java
@@ -0,0 +1,11 @@
+public class Ben{
+    public static void main(String args[]){
+        if(false){
+            System.out.println(false);
+
+        }
+        if(true){
+            System.out.println(true);
+        }
+    }
+}
