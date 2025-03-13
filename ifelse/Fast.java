86325323411bbc3a85c20ce1f1005aa818ca0d6a
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 11:05:12 +0530

    program was excuted sucessfully

---
+++ ifelse/Fast.java
@@ -0,0 +1,10 @@
+public class Fast{
+    public static void main(String args[]){
+        if(true){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}   
