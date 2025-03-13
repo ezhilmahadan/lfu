90291dedc9540ef8694ae4c5d9f3ac6a0ac8a1e7
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 11:43:39 +0530

    program was excuted sucessfully

---
+++ ifelse/Ez.java
@@ -0,0 +1,12 @@
+public class Ez{
+    int a;
+    public static void main(String[] args){
+        int a = 20;
+        if(a>0){
+            System.out.println("a is postive");
+        }
+        else{
+            System.out.println("a is negative");
+        }
+    }
+}
