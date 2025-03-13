65d089f416f40fe43ccd53d84bcb4b03dcab2a56
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 10:37:49 +0530

    program was excuted sucessfully

---
+++ condtionaloperator/Math.java
@@ -0,0 +1,10 @@
+public class Math{
+    public static void main(String args[]){
+        System.out.println(10<20);
+        System.out.println(10>20);
+        System.out.println(2<=5);
+        System.out.println(2>=5);
+        System.out.println(11!=12);
+        System.out.println(11==11);
+    }
+}
