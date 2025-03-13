12cd0bc42bb01351463dd726bfba9c18c8e38d95
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 11:49:30 +0530

    program was sucessfully excuted

---
+++ ifelse/Ok.java
@@ -0,0 +1,15 @@
+public class Ok{
+    int a;
+    public static void main(String[] args){
+        int a = -7;
+        if(a>0){
+            System.out.println("a is postive");
+        }
+        else if(a==0){
+            System.out.println("a is neutral");
+        }
+        else{
+            System.out.println("a is negative");
+        }
+    }
+}
