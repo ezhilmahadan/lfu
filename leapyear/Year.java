eddcb7fdefdddb002af7726dbdefaf7fc9f61397
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-13 14:32:02 +0530

    program was excuted sucessfully

---
+++ leapyear/Year.java
@@ -0,0 +1,12 @@
+public class Year{
+    int i;
+    public static void main(String[] args){
+        int i = 2020;
+        if(i%4==0){
+            System.out.println("given year is leap year");
+        }
+        else{
+            System.out.println("given year is not leap year");
+        }
+    }
+}
