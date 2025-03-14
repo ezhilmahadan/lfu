19a1481ba20916e5327252d96ec7b26f6ad20b0a
ezhilmahadan <ezhilmadan@gmail.com>
2025-03-14 10:04:00 +0530

    program was sucessfully excuted

---
+++ while/Stack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Stack{
+    public static void main(String[] args){
+        int i=0;
+        while(i<10){
+            i=i+1;
+        System.out.println(i);
+        }
+    }
+}
